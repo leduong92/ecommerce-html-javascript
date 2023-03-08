46af192d0aed7c36c6f31c50abd94212f471cd53
Duong <ltson@theodorealexander.com>
2023-03-08 15:11:01 +0700

    new project

---
+++ script.js
@@ -0,0 +1,47 @@
+//copy menu for mobild
+function copyMenu() {
+  //copy inside .dpt-cat to departments
+  let dptCategory = document.querySelector(".dpt-cat");
+  let dptPlace = document.querySelector(".departmets");
+  dptPlace.innerHTML = dptCategory.innerHTML;
+
+  //copy inside nav to nav
+  let mainNav = document.querySelector(".header-nav nav");
+  let navPlace = document.querySelector(".off-canvas nav");
+  navPlace.innerHTML = mainNav.innerHTML;
+
+  //copy .header-top .wrapper to .thetop-nav
+  let topNav = document.querySelector(".header-top .wrapper");
+  let topPlace = document.querySelector(".off-canvas .thetop-nav");
+  topPlace.innerHTML = topNav.innerHTML;
+}
+
+copyMenu();
+
+//show mobile menu
+const menuButton = document.querySelector(".trigger"),
+  closeButton = document.querySelector(".t-close"),
+  addclass = document.querySelector(".site");
+
+menuButton.addEventListener("click", function () {
+  addclass.classList.toggle("showmenu");
+});
+
+closeButton.addEventListener("click", function () {
+  addclass.classList.remove("showmenu");
+});
+
+//show sub menu on mobile
+const submenu = document.querySelectorAll(".has-child .icon-small");
+submenu.forEach((menu) => menu.addEventListener("click", toggle));
+
+function toggle(e) {
+  e.preventDefault();
+  submenu.forEach((item) =>
+    item != this ? item.closest(".has-child").classList.remove("expand") : null
+  );
+
+  if (this.closest(".has-child").classList != "expand") {
+    this.closest(".has-child").classList.toggle("expand");
+  }
+}
